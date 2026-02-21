cb10297d4e78dc4ce931c3ecbdd55805a8120fca
John Carraher <jkcarraher@gmail.com>
2026-02-20 17:02:23 -0800

    Task #1: Created controllers for show tables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ShowsModule } from './shows/shows.module';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    ShowsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
