1a5f8983e6ae8450aa066addd0de322aecb8836b
John Carraher <jkcarraher@gmail.com>
2026-05-22 13:40:39 -0700

    Updating compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,10 @@ import { ShowsModule } from './shows/shows.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'db',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
+      host: process.env.DB_HOST || 'db',
+      port: parseInt(process.env.DB_PORT!, 10) || 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
       database: 'postgres',
       entities: [],
       synchronize: true,
