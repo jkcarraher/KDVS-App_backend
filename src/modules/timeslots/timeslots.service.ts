991bf9f625fffc97d31a2a103960aec1711b7ab1
John Carraher <jkcarraher@gmail.com>
2026-06-19 14:47:13 -0700

    Revert

--- src/modules/timeslots/timeslots.service.ts
+++ src/modules/timeslots/timeslots.service.ts
@@ -125,7 +125,7 @@ export class TimeslotsService {
 
       return (
         weeksSinceAnchor >= 0 &&
-        weeksSinceAnchor % (slot.recurrence_interval_weeks-1) === 0
+        weeksSinceAnchor % (slot.recurrence_interval_weeks) === 0
       );
     });
     }
